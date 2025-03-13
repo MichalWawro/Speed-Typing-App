136ea1c474a2e1b5587c4150149af90bff4f6e05
Michał Wawro <mwawro77@gmail.com>
2025-03-14 00:04:02 +0100

    Setup bug fixes

---
+++ server/src/main/java/com/example/typingtestgame/TypingTestGameApplication.java
@@ -0,0 +1,15 @@
+package com.example.typingtestgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TypingTestGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingTestGameApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ server/src/test/java/com/example/typingtestgame/TypingTestGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typingtestgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingTestGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
