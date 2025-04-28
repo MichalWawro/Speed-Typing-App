979d4b2214c51b2a7c57c3d47496fc6336b6e54d
Michał Wawro <mwawro77@gmail.com>
2025-04-28 22:53:44 +0200

    Updated word randomizer and word list

--- server/src/main/java/com/example/typingtestgame/controller/RandomTextController.java
+++ server/src/main/java/com/example/typingtestgame/controller/RandomTextController.java
@@ -23,24 +23,7 @@ public class RandomTextController {
     @GetMapping("/get-random-array")
     @CrossOrigin(origins = {"http://localhost:3000"})
     public ResponseEntity<List<String>> getRandomWords(
-            @RequestParam(defaultValue = "75") int count) {
+            @RequestParam(defaultValue = "200") int count) {
         return ResponseEntity.ok(randomTextService.getRandomWords(count));
     }
-
-    @GetMapping("/get-set-array")
-    @CrossOrigin(origins = {"http://localhost:3000"})
-    public ArrayList getArrayText() {
-        return new ArrayList<>(Arrays.asList("car", "dog", "house", "milk",
-                "chair", "window", "wisdom", "phone", "book", "shoe", "apple", "train", "school", "cat", "hat", "door",
-                "glass", "bottle", "pen", "cup", "bed", "bread", "monkey", "clock", "banana", "couch", "floor", "cloud",
-                "sun", "moon", "table", "tree", "family", "star", "river", "lake", "shirt", "pants", "sock", "ball", "cake",
-                "flower", "mountain", "road", "bus", "farm", "field", "toothbrush", "pillow", "snow", "rain", "sunset", "sky", "house", "people", "think", "or", "no"));
-    }
 }
-//    @CrossOrigin(origins = {"http://localhost:3000", "http://WebIP:Port"})
-//        return ("bread monkey table tree family milk sunset sky house people think or no " +
-//                "old fat wide before always never blue fly click echo empty whisper time strange " +
-//                "smile mirror howl memory from promise shadow and one deer deep silent soft rain low cold hot" +
-//                "bread monkey table tree family milk sunset sky house people think or no " +
-//                "old fat wide before always never blue fly click echo empty whisper time strange " +
-//                "smile mirror howl memory from promise shadow and one deer deep silent soft rain low cold hot");
